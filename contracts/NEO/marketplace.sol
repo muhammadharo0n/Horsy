8ef99bf60442ad858bef32dc2d94cf8f4eb75490
muhammadharo0n <haroonworka768@gmail.com>
2024-06-12 11:07:37 +0500

    12-June

--- contracts/NEO/marketplace.sol
+++ contracts/NEO/marketplace.sol
@@ -8,26 +8,26 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-interface IConnected {
-    // Struct to encapsulate detailed information about an NFT, used for easy data retrieval.
-    struct NFT{ 
-        uint256 tokenId;    
-        uint256 price;
-        uint256 count;
-        bool minted;
-        address artist;
-        string uri;
-        uint mintTime;
-    }
+// interface IConnected {
+//     // Struct to encapsulate detailed information about an NFT, used for easy data retrieval.
+//     struct NFT{ 
+//         uint256 tokenId;    
+//         uint256 price;
+//         uint256 count;
+//         bool minted;
+//         address artist;
+//         string uri;
+//         uint mintTime;
+//     }
 
 
-    // Functions to be implemented by connected contracts for updating and retrieving NFT data
-    function updateTokenId(address _to,uint _tokenId,address seller) external;
-    // function update_TokenIdTime(uint _tokenId) external;
-    function getTokenId(address _address) external view returns(NFT [] memory);
-    function getTokenUri(uint _tokenId) external view returns(string memory);
+//     // Functions to be implemented by connected contracts for updating and retrieving NFT data
+//     function updateTokenId(address _to,uint _tokenId,address seller) external;
+//     // function update_TokenIdTime(uint _tokenId) external;
+//     function getTokenId(address _address) external view returns(NFT [] memory);
+//     function getTokenUri(uint _tokenId) external view returns(string memory);
 
-}
+// }
 contract NFTMarketplace is Ownable {
 
     using Counters for Counters.Counter;
@@ -45,7 +45,12 @@ contract NFTMarketplace is Ownable {
         address mintContract;
         uint tokenId;
     }
-
+    
+    mapping(uint=>mapping(address=>NFT)) public Tier1;
+    mapping(uint=>mapping(address=>NFT)) public Tier2;
+    mapping(uint=>mapping(address=>NFT)) public Tier3;
+    mapping(uint=>mapping(address=>NFT)) public Tier4;
+    mapping(uint=>mapping(address=>NFT)) public Tier5;
     mapping(address mintContract=>mapping(uint tokenId => NFT)) public NftListing;
     mapping(uint=>Index) public IndexListing; 
 
@@ -64,19 +69,37 @@ contract NFTMarketplace is Ownable {
         NftListing[_mintContract][_tokenId] = NFT(_tokenId, _price, true, msg.sender, _uri);
         IndexListing[_itemIds.current()] = Index(_mintContract, _tokenId);
         emit Listing (_tokenId , msg.sender , _price, _uri, true);
-    }
+        require(_tokenId <= 15, 'No more NFT to mint');
 
-    function buy (uint _tokenId) public payable { 
+    if(_tokenId <= 5){ 
+        Tier1[_tokenId][msg.sender] = NFT(_tokenId, _price, true, msg.sender, _uri);
         
-        address contractAddress = IndexListing[_tokenId].mintContract;
-        require(NftListing[contractAddress][_tokenId].owner!= msg.sender, "Owner cannot buy this NFT");
-        require(NftListing[contractAddress][_tokenId].minted= true, "NFT not Listed");
-        ERC721(contractAddress).transferFrom(address(this), msg.sender, _tokenId);
+    } else if(_tokenId <= 9){
 
-        payable(owner()).transfer(msg.value);
-        emit NftSold(_tokenId, msg.sender, NftListing[contractAddress][_tokenId].price, NftListing[contractAddress][_tokenId].uri, NftListing[contractAddress][_tokenId].minted);
+        Tier2[_tokenId][msg.sender] = NFT(_tokenId, _price, true, msg.sender, _uri);  
+         
+    } else if(_tokenId <= 12){
+        Tier3[_tokenId][msg.sender] = NFT(_tokenId, _price, true, msg.sender, _uri);
+    }
+      else if(_tokenId <= 14){
+        Tier4[_tokenId][msg.sender] = NFT(_tokenId, _price, true, msg.sender, _uri);
+    }
+      else if(_tokenId <= 15){
+        Tier5[_tokenId][msg.sender] = NFT(_tokenId, _price, true, msg.sender, _uri);
+    }
+        ERC721(_mintContract).transferFrom(msg.sender, address(this), _tokenId);
     }
 
+    function buy (uint tokenId) public payable { 
+        
+        address contractAddress = IndexListing[tokenId].mintContract;
+        require(NftListing[contractAddress][tokenId].owner!= msg.sender, "Owner cannot buy this NFT");
+        require(NftListing[contractAddress][tokenId].minted= true, "NFT not Listed");
+        require(msg.value >= NftListing[contractAddress][tokenId].price, "Insufficient funds");
+        ERC721(contractAddress).transferFrom(address(this), msg.sender, IndexListing[tokenId].tokenId);
 
+        payable(owner()).transfer(NftListing[contractAddress][tokenId].price);
+        emit NftSold(tokenId, msg.sender, NftListing[contractAddress][tokenId].price, NftListing[contractAddress][tokenId].uri, NftListing[contractAddress][tokenId].minted);
+    }
 
 }
