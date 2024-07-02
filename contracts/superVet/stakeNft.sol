c1ab021f030af40191069ba3b4795c3fbe8cbad9
muhammadharo0n <haroonworka768@gmail.com>
2024-07-02 15:55:36 +0500

    2-July

--- contracts/superVet/stakeNft.sol
+++ contracts/superVet/stakeNft.sol
@@ -4,11 +4,12 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 
 contract Marketplace is Ownable{
-    
+        
+    using SafeERC20 for IERC20;
     uint256 public _nextTokenId;
     uint public _nftStakeId;
     uint tokenAddress;
@@ -20,19 +21,6 @@ contract Marketplace is Ownable{
 
 
     }
-        
-    struct NftListing{
-        uint tokenId;
-        uint price;
-        address to;
-        address artist;
-        bool sold;
-        bool listed;
-    }
-    struct TokenAddress{ 
-        uint tokenId;
-        address mintContract;
-    }
     struct NftStaking{ 
         uint tokenId;
         uint startTime;
@@ -42,33 +30,18 @@ contract Marketplace is Ownable{
     struct stakingNftIndex{ 
         uint tokenId;
         address mintContract;
-    }
-    mapping(address mintContractAddress => mapping (uint tokenId => NftListing) ) public tokenListing;
-    mapping(uint => TokenAddress) public Index;
+    }   
+
     mapping(address  => NftStaking) public stakeListing;
     mapping(uint => stakingNftIndex) public stakingIndex;
 
-
-    function listTokens (uint _tokenId, uint _price, address _mintContract) public{ 
-        _nextTokenId++;
-        tokenListing[_mintContract][_tokenId]= NftListing(_tokenId, _price, address(this), msg.sender,false, true);
-        Index[_nextTokenId] = TokenAddress(_tokenId, _mintContract);
-        ERC721(_mintContract).transferFrom(msg.sender,address(this), _tokenId);      
+    function tokenDeposit(uint amount, address _tokenAddress) public onlyOwner{ 
+        IERC20(_tokenAddress).safeTransferFrom(msg.sender, address(this), amount);
     }
-
-    function buyTokens( uint _tokenId) public payable {
-
-        address contractAddress = Index[_tokenId].mintContract;
-        require(tokenListing[contractAddress][_tokenId].price <= msg.value,"you have entred wrong price ");
-        require(tokenListing[contractAddress][_tokenId].listed = true , "NFT not minted");
-        ERC721(contractAddress).safeTransferFrom(address(this), msg.sender, _tokenId);
-        payable (owner()).transfer(msg.value);
-
-        } 
     function stakeNft( uint _tokenId, address contractAddress) public { 
         
         _nftStakeId++;
-        stakeListing[msg.sender] = NftStaking(_tokenId, block.timestamp, contractAddress);
+        stakeListing[msg.sender] = NftStaking(_tokenId, block.timestamp, contractAddress, true);
         ERC721(contractAddress).transferFrom(msg.sender,address(this), _tokenId);
         stakingIndex[_nftStakeId] = stakingNftIndex(_tokenId, contractAddress);
    
