b7f305df44634d9fb33661e25dca9040f9447529
muhammadharo0n <haroonworka768@gmail.com>
2024-07-15 15:22:40 +0500

    Fatcat changes

---
+++ contracts/fatcat/mintFatcat.sol
@@ -0,0 +1,221 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+import "@openzeppelin/contracts@4.9.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.9.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.9.0/utils/Counters.sol";
+/// @title Marketplace  for the NFTS
+/// @author FatCat Team
+/// @notice Contarct is based on minting the Nfts
+interface Auction {
+    function AuctionOfferList(address _mintContract,uint _tokenId,uint _minPrice,address artist,uint artistFeePerAge) external;
+}
+contract Minting is ERC721URIStorage, Ownable {
+    // Marketplace Address 
+    address public marketplaceAddress;
+    // Utility from OpenZeppelin for safely incrementing and decrementing numbers.
+    using Counters for Counters.Counter;
+    // A Counter to keep track of the last used token ID; ensures each token has a unique ID.
+    Counters.Counter public _tokenIdCounter;
+    // Struct to store an array of token IDs associated with a specific collection ID.
+    struct TokenIdByCollection {
+        uint256[] tokenIds;
+    }
+    string collectionName; // Collection Name
+    string collectionSymbol; //Collection Symbol of the collection Id
+    string Network; // Collection Network
+    // Struct to store metadata for each NFT minted.
+    struct NFT {
+        uint256 mintTime; // Timestamp of when the NFT was minted.
+        address mintArtist; // Address of the artist who minted the NFT.
+        uint artistFeePerAge; // A fee related to the artist, possibly a royalty or similar concept.
+    }
+    
+    // Struct to encapsulate detailed information about an NFT, used for easy data retrieval.
+    struct MyNft {
+        uint256 tokenId; // Unique identifier for the NFT.
+        uint256 mintTime; // Timestamp of the minting.
+        address mintContract; // Address of the contract where the NFT was minted.
+        address mintArtist; // Address of the artist who minted the NFT.
+        uint artistFeePerAge; // Fee associated with the artist as per their age(?) or duration since minting.
+        string uri; // URI for the NFT's metadata.
+    }
+    //mapping(address  => mapping(uint  => nftAuction)) public NftAuction;  
+    // Nested mapping to store which token IDs belong to which address, with a sequential index for enumeration.
+    mapping(address => mapping(uint256 => uint256)) public TokenId;
+    // Mapping to store the count of NFTs owned by each address.
+    mapping(address => uint256) public count;
+    // Mapping from collection IDs to their respective token IDs, facilitating collection-based operations.
+    mapping(string => TokenIdByCollection) private tokenIdByCollection;
+    // Mapping from token ID to its corresponding NFT metadata, for easy lookup.
+    mapping (uint => NFT) public NFTMetadata;
+    // Mapping from Address to nftTransfer Recieved
+    mapping (address => uint32) public userTransferRecievedCount;
+    // Administrative address, likely used for contract management or privileged operations.
+    address public adminAddress;
+    // Event emitted when an NFT is successfully minted, including details like tokenId, minter, and mint time.
+    event SafeMinting(uint256 tokenId, address Minter, uint MintingTime);
+    // Constructor setting the initial admin address and initializing the ERC721 token with a name and symbol.
+    constructor(address _adminAddress,string memory _collectionName,string memory _collectionSymbol, string memory _Network, address _marketplaceAddress) ERC721("FATCAT", "CAT") {
+        adminAddress = _adminAddress;
+        collectionName = _collectionName;
+        collectionSymbol = _collectionSymbol;
+        Network = _Network;
+        marketplaceAddress = _marketplaceAddress;
+    }
+    /**
+    * @dev Allows a user to mint a new NFT.
+    * @param uri The URI for the NFT metadata.
+    * @param artist The address of the artist creating the NFT.
+    * @param artistFeePerAge A specified fee related to the artist, potentially for royalties or similar.
+    * @param collectionId The ID of the collection to which this NFT will be added.
+    *
+    * This function mints a new NFT to the caller's address, assigns it a unique token ID,
+    * sets its metadata URI, records its minting time, artist, and artist fee, and associates it with a collection.
+    * Emits a SafeMinting event upon successful minting.
+    */
+    function safeMint(string memory uri,address artist,uint artistFeePerAge,string memory collectionId) public {
+        _tokenIdCounter.increment();
+        TokenId[msg.sender][count[msg.sender] + 1] = _tokenIdCounter.current();
+        _safeMint(msg.sender, _tokenIdCounter.current());
+        _setTokenURI(_tokenIdCounter.current(), uri);
+        count[msg.sender]++;
+        Auction(marketplaceAddress).AuctionOfferList(address(this),_tokenIdCounter.current(),0,msg.sender,artistFeePerAge);
+        NFTMetadata[_tokenIdCounter.current()] = NFT(block.timestamp,artist,artistFeePerAge);
+        tokenIdByCollection[collectionId].tokenIds.push(_tokenIdCounter.current());
+        emit SafeMinting(_tokenIdCounter.current(),msg.sender,block.timestamp);
+    }
+    /**
+    * @dev Retrieves a list of NFTs owned by a given address.
+    * @param to The address to query for owned NFTs.
+    * @return An array of MyNft structs, each representing an NFT owned by the queried address.
+    *
+    * This function constructs an array of MyNft structs containing detailed information
+    * about each NFT owned by the specified address, including the token ID, mint time,
+    * contract address, minting artist, artist fee, and metadata URI.
+    */
+    function getTokenId(address to) public view returns (MyNft[] memory) {
+        MyNft[] memory myArray = new MyNft[](count[to]);
+        for (uint256 i = 0; i < count[to]; i++) {
+            myArray[i] = MyNft(TokenId[to][i + 1],NFTMetadata[TokenId[to][i + 1]].mintTime,address(this),NFTMetadata[TokenId[to][i + 1]].mintArtist,NFTMetadata[TokenId[to][i + 1]].artistFeePerAge,tokenURI(TokenId[to][i + 1]));
+        }
+        return myArray;
+    }
+    /**
+    * @dev Transfers a token ID from one owner to another and updates internal tracking.
+    * @param _to The address to receive the token ID.
+    * @param _tokenId The token ID to be transferred.
+    * @param _seller The current owner of the token ID.
+    *
+    * This function updates the internal mapping of token IDs to reflect a change in ownership.
+    * It also adjusts the count of NFTs owned by both the seller and the buyer.
+    * Note: This function does not perform the actual transfer of tokens but is intended to be called
+    * in conjunction with a transfer function that handles ownership change.
+    */
+    function updateTokenId(address _to,uint _tokenId,address _seller) public {
+        TokenId[_to][count[_to] + 1] = _tokenId;
+        MyNft[] memory myArray =  getTokenId(_seller);
+        for(uint i=0 ; i < myArray.length ; i++){
+            if(myArray[i].tokenId == _tokenId){
+                TokenId[_seller][i+1] = TokenId[_seller][count[_seller]];
+                count[_seller]--;
+            }
+        }
+        count[_to]++;
+    }
+    /**
+    * @dev Updates the mint time of a specific token ID to the current block timestamp.
+    * @param _tokenId The token ID for which to update the mint time.
+    *
+    * This could be used to refresh or update the timestamp associated with an NFT,
+    * potentially for mechanisms that depend on the age or recency of minting.
+    */
+    function update_TokenIdTime(uint _tokenId) external {
+        NFTMetadata[_tokenId].mintTime = block.timestamp;
+    }
+    // The following functions are overrides required by Solidity.
+    function _burn(uint256 tokenId)
+        internal
+        override
+    {
+        super._burn(tokenId);
+    }
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+    function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override   {
+        updateTokenId(to,tokenId,from);
+        super.safeTransferFrom(from, to, tokenId);
+        userTransferRecievedCount[to]++;
+    }
+    /**
+    * @dev Retrieves all NFTs associated with a given collection ID.
+    * @param collectionId The ID of the collection for which to retrieve NFTs.
+    * @return An array of MyNft structs, each representing an NFT in the specified collection.
+    * This function iterates through all token IDs associated with the given collection ID,
+    * constructing an array of MyNft structs that includes detailed information about each NFT,
+    * such as token ID, mint time, the contract address, the minting artist, artist fee, and the token's metadata URI.
+    */
+    function getTokenIdsByCollection(string memory collectionId)
+        public
+        view
+        returns (MyNft[] memory)
+    {
+        uint256[] memory tokenIds = tokenIdByCollection[collectionId].tokenIds;
+        MyNft[] memory myArray = new MyNft[](tokenIds.length);
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            uint256 currentTokenId = tokenIds[i];
+            myArray[i] = MyNft(currentTokenId,NFTMetadata[currentTokenId].mintTime,address(this),NFTMetadata[currentTokenId].mintArtist,NFTMetadata[currentTokenId].artistFeePerAge,tokenURI(currentTokenId));
+        }
+        return myArray;
+    }
+    /**
+    * @dev Returns the contract's current balance.
+    * @return The balance of the contract in wei.
+    *
+    * This function queries the contract's balance, allowing visibility into the amount of Ether held by the contract.
+    */
+    function contractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+    /**
+    * @dev Allows the current admin to transfer admin rights to a new address.
+    * @param _adminAddress The address to be granted admin rights.
+    *
+    * This function updates the admin address of the contract. It includes a security check to ensure
+    * that only the current admin can perform this operation, enhancing the contract's security by preventing
+    * unauthorized changes to administrative privileges.
+    */
+    function setAdminAddress(address _adminAddress) external {
+        require(adminAddress==msg.sender,"You are not Admin");
+        adminAddress = _adminAddress;
+    }
+    /**
+    * @dev Retrieves the metadata URI for a specific token ID.
+    * @param tokenId The token ID for which to retrieve the metadata URI.
+    * @return The URI string of the requested token's metadata.
+    *
+    * This function provides a way to access the metadata URI associated with a given token ID,
+    * allowing external entities and interfaces to retrieve the metadata for display or processing purposes.
+    */
+    function getTokenUri(uint tokenId) external view returns(string memory){
+        return tokenURI(tokenId);
+    }
+        // The following functions are overrides required by Solidity.
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+    function getColletionDetails () public view returns(string memory, string memory,string memory){
+        return (collectionName,collectionSymbol,Network);
+    }
+}
--- contracts/practiceProject/marketPlace.sol
+++ contracts/practiceProject/marketPlace.sol
@@ -7,12 +7,14 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTMarketplace is Ownable {
-
     using Counters for Counters.Counter;
-    Counters.Counter private _itemIds;
-    Counters.Counter private itemsAuction;
-
-    constructor(address owner) Ownable(msg.sender){
+    IERC721 public landContract;
+    // State variables
+    Counters.Counter public nftAuctionCount;       // Counter for NFTs listed for auction
+    Counters.Counter public nextLandListId;       // Counter for LandNFTs listed in marketplace
+    Counters.Counter public nextBundleListingId;
+    constructor(address owner, IERC721 _landMinting) Ownable(msg.sender){
+    landContract = _landMinting;
 
     owner = msg.sender;
 
@@ -31,19 +33,7 @@ contract NFTMarketplace is Ownable {
         uint tokenId;
         address owner;
     }
-    struct Auction{ 
-        uint tokenId;
-        uint amount;
-        uint minBid;
-        uint artistFeePerAge;
-        uint endTime;
-        uint highestBid;
-        address owner;
-        address artist;
-        address highestBidder;
-        bool isActive;
 
-    }
     struct auctionIndexing{
         uint tokenId;
         address contractAddress;
@@ -60,54 +50,195 @@ contract NFTMarketplace is Ownable {
         uint priceEntered;
     }
 
+
+
+        struct ListLand {
+        address owner;
+        address seller;
+        uint256 tokenId;
+        uint256 count;
+        uint256 price;
+        bool listed;
+    
+    } 
+
+    struct BundleListing {
+        address seller;
+        address owner;
+        address artist;
+        uint256[] tokenIds;
+        uint256[] amounts;
+        uint256 price;
+        uint artistFee;
+        bool active;
+    }
+
+    struct Auction {
+        address owner;          // Owner of the NFT being auctioned.
+        uint tokenId;           // Unique identifier for the NFT.
+        uint amount;
+        uint minimumBid;        // Minimum bid required to participate in the auction.
+        address artist;         // The original creator/artist of the NFT.
+        uint artistFeePerAge;   // Artist's fee per age, similar to the NFT struct.
+        uint endTime;
+        bool isActive;          // Indicates if the auction is currently active.
+        address highestBidder;
+        uint highestBid;
+    }
+    struct NftDetails{ 
+        uint TokenId;
+        address stakerAddress; 
+        address currentOwnerAddress; 
+        uint userWithdrawToken;
+        uint withdrawMonth;
+        uint stakeTime;
+        bool isActive;
+    }
+
+
+    struct AddressToken {
+        address contractAddress;
+        uint tokenId;
+        uint amount;
+    }
+    struct addressToken {
+        address contractAddress;
+        uint[] tokenId;
+        uint[] amount;
+    }
+    struct Address_Token {
+        address contractAddress;
+        uint tokenId;
+    }
+
+     // Stores details about a user's bid in an auction, including the bid amount and time.
+    struct userDetail {
+        address user;           // Address of the user making the bid.
+        string userName;        // Optionally, a username or identifier for the bidder.
+        uint price;             // The price of the bid.
+        uint biddingTime;       // Timestamp when the bid was placed.
+        uint bidCount;          // Number of bids placed by this user (for this auction?).
+    }
+     // Contains information about a Auction NFT in the auction, including its data and listing count.
+    struct ListTokenId {
+        Auction listedData;   // The auction data for the listed NFT.
+        uint listCount;          // A count that could represent the number of times listed or an ID.
+        string uriData;          // URI for the NFT metadata.
+    }
+
+    // Similar to `ListTokenId` but specifically for NFTs listed for direct sale.
+    struct ListedNftTokenId {
+        BundleListing listedData;          // The direct sale listing data for the NFT.
+        uint listCount;          // A count or ID similar to `ListTokenId`.
+        string uriData;          // URI for the NFT metadata.
+    }
+    struct ListedLandNftTokenId {
+        ListLand listedData;          // The direct sale listing data for the NFT.
+        uint listCount;          // A count or ID similar to `ListTokenId`.
+        string uriData;          // URI for the NFT metadata.
+    }
+
     mapping(address => mapping (uint => NFTItem)) public nftListing;
     mapping(uint =>Index) public nftIndex;
 
     mapping(address => mapping(uint =>Auction))public autionListing;
     mapping(uint => auctionIndexing) public auctionIndex;
-    mapping(address => mapping (uint => bidder)) public auctionBidder;
-    mapping(address=>uint) public userBidCounter;
+    mapping(address => mapping (uint => mapping(uint=> bidder))) public auctionBidder;
+    mapping(uint=>uint) public userBidCounter;
+
+    mapping(address => uint256) public userCount;
+    mapping(uint => uint) public userListCount; 
+    mapping(uint => AddressToken) public auctionListCount;                        // Maps auction indices to address and token ID pairs
+    mapping(address => mapping(uint => mapping(uint => userDetail))) public Bidding; // Maps auction details to user bids
+    mapping(uint => address) public SelectedUser;                                    // Maps selected user for auction
+    mapping(address => mapping(uint => mapping(address => mapping(uint => uint)))) public BiddingCount; // Helper mapping for bidding counts
+    mapping(address => mapping(uint => mapping(address => uint))) public userBiddingCount; // Helper mapping for user bidding counts
+    mapping(address => mapping(uint => Auction)) public NftAuction; 
+    mapping (address mintAddress => mapping (uint tokedId => NftDetails)) public NftSupply;
+    mapping (address => mapping (uint =>uint)) public rewardAmount;
+    mapping(address => mapping(uint256 => BundleListing)) public userBundleListings;
+    mapping(address => mapping(uint256 => ListLand)) public userLandListings;
 
     event ItemCreated(uint256 indexed itemId, address indexed creator, string uri, uint256 price);
     event ItemSold(uint256 indexed itemId, address indexed buyer, uint256 price);
 
-    function listItem(address _mintContract, uint tokenId, string memory uri, uint256 price) public onlyOwner {
-        _itemIds.increment();
-        nftListing[_mintContract][tokenId]= NFTItem(tokenId, price, _itemIds.current(),msg.sender, uri, true);
-        nftIndex[tokenId] = Index(_itemIds.current(), msg.sender);
-        ERC721(_mintContract).transferFrom(msg.sender, address(this), tokenId); 
-        emit ItemCreated(_itemIds.current(), msg.sender, uri, price);
-    }
-
-    function auctionNft(address _mintContract, uint tokenId, uint _minimumBid, uint _artistFee, address _artist) public{ 
-        itemsAuction.increment();
-        autionListing[_mintContract][tokenId] = Auction(tokenId, itemsAuction.current(), _minimumBid, _artistFee, block.timestamp + 100 seconds, 0,msg.sender, _artist, address(0), true);
-        auctionIndex[tokenId] = auctionIndexing(itemsAuction.current(), _mintContract);
-        ERC721(_mintContract).transferFrom(msg.sender, address(this), tokenId);
-    }
-    function biddingNft(uint _tokenId, string memory _username) public { 
-        address contractAddress = auctionIndex[_tokenId] .contractAddress;
-        uint tokenId = auctionIndex[_tokenId] .tokenId;
-        require(autionListing[contractAddress][tokenId].minBid<=autionListing[contractAddress][tokenId].amount,"Bid should be more that Minimum bid ");
-        // require(autionListing[contractAddress][tokenId].isActive,"tokenId is not active yet");
-        require(autionListing[contractAddress][tokenId].artist != msg.sender,"artist canot bid");
-        require(autionListing[contractAddress][tokenId].owner != msg.sender,"owner cannot bid");
-        autionListing[contractAddress][tokenId].highestBid >= autionListing[contractAddress][tokenId].amount;
-        autionListing[contractAddress][tokenId].highestBidder == msg.sender;
-        
-        auctionBidder[contractAddress][_tokenId] = bidder(msg.sender, _username, block.timestamp,userBidCounter[msg.sender]++, autionListing[contractAddress][tokenId].highestBid);
-
-    }
-    // function buyItem(uint256 itemId) public payable {
-    //     NFTItem storage item = _items[itemId];
-    //     require(item.sold == false, "Item already sold");
-    //     require(msg.value >= item.price, "Insufficient funds");
-
-    //     item.sold = true;
-    //     _itemsSold.increment();
-
-    //     emit ItemSold(itemId, msg.sender, item.price);
-
-    //     payable(item.creator).transfer(item.price);
+    // function listItem(address _mintContract, uint tokenId, string memory uri, uint256 price) public onlyOwner {
+    //     _itemIds.increment();
+    //     nftListing[_mintContract][tokenId]= NFTItem(tokenId, price, _itemIds.current(),msg.sender, uri, true);
+    //     nftIndex[tokenId] = Index(_itemIds.current(), msg.sender);
+    //     ERC721(_mintContract).transferFrom(msg.sender, address(this), tokenId); 
+    //     emit ItemCreated(_itemIds.current(), msg.sender, uri, price);
     // }
+
+    // function auctionNft(address _mintContract, uint tokenId, uint _minimumBid, uint _artistFee, address _artist) public{ 
+    //     itemsAuction.increment();
+    //     autionListing[_mintContract][tokenId] = Auction(tokenId, itemsAuction.current(), _minimumBid, _artistFee, block.timestamp + 100 seconds, 0,msg.sender, _artist, address(0), true);
+    //     auctionIndex[tokenId] = auctionIndexing(itemsAuction.current(), _mintContract);
+    //     ERC721(_mintContract).transferFrom(msg.sender, address(this), tokenId);
+    // }
+    // function biddingNft(uint _tokenId, string memory _username, uint _price) public { 
+    //     address contractAddress = auctionIndex[_tokenId] .contractAddress;
+    //     uint tokenId = auctionIndex[_tokenId] .tokenId;
+    //     require(_price>=autionListing[contractAddress][tokenId].minBid,"Bid should be more that Minimum bid ");
+    //     // require(autionListing[contractAddress][tokenId].isActive,"tokenId is not active yet");
+    //     // require(autionListing[contractAddress][tokenId].artist != msg.sender,"artist canot bid");
+    //     // require(autionListing[contractAddress][tokenId].owner != msg.sender,"owner cannot bid");
+    //     autionListing[contractAddress][tokenId].highestBid = _price;
+    //     autionListing[contractAddress][tokenId].highestBidder = msg.sender;
+    //     userBidCounter[tokenId]++;
+    //     auctionBidder[contractAddress][_tokenId][userBidCounter[tokenId]++] = bidder(msg.sender, _username, block.timestamp,userBidCounter[tokenId]++, _price);
+
+    // }
+    function OfferList(address _mintContract, uint _tokenId, uint amount, uint _maxPrice, address artist, uint artistFee) external {
+        require(!userBundleListings[_mintContract][_tokenId].active, "Already Listed In Marketplace!");
+        require(!NftAuction[_mintContract][_tokenId].isActive, "Already Listed In Auction!");
+        nftAuctionCount.increment();
+        NftAuction[_mintContract][_tokenId] = Auction(msg.sender, _tokenId, amount, _maxPrice, artist, artistFee, block.timestamp + 10 minutes, true, address(0), 0);
+        auctionListCount[nftAuctionCount.current()] = AddressToken(_mintContract, _tokenId, amount);
+        userCount[msg.sender] = 0;
+        ERC721(_mintContract).transferFrom(msg.sender, address(this), _tokenId);
+    }
+
+    /**
+    * @dev Allows users to place bids on NFTs that are listed for auction.
+    *
+    * Participants can bid on NFTs by specifying the auction they wish to participate in, their name,
+    * and the bid amount. This function updates the auction state with the new bid details.
+    *
+    * @param _auctionListCount The index of the auction in the `auctionListCount` mapping, indicating
+    *                          which NFT the bid is for. This index helps identify the specific auction.
+    * @param _name The name of the bidder. This parameter can be used for identification or display purposes
+    *              in a UI.
+    * @param _price The amount of the bid placed by the user. This value must be higher than the current
+    *               highest bid for the auction to be considered valid.
+    *
+    * Requirements:
+    * - The caller (bidder) must not be the owner of the NFT. Owners cannot bid on their own NFTs.
+    * - The auction for the NFT must be active. Bids cannot be placed on NFTs not listed for auction or
+    *   after the auction has ended.
+    * 
+    * The function performs the following operations:
+    * 1. Retrieves the contract address and token ID of the NFT being bid on, based on `_auctionListCount`.
+    * 2. Validates that the caller is not the owner of the NFT and that the auction is active.
+    * 3. Increments the count of bids placed by the user for this specific NFT.
+    * 4. Records the new bid in the `Bidding` mapping, which stores all bids for each auction.
+    * 5. Updates the user's bidding count and the overall list of bids for this auction.
+    *
+    * This setup ensures that bids are accurately tracked and associated with the correct auction and bidder.
+    * It allows for a transparent bidding process where all participants can place bids until the auction
+    * concludes.
+    */
+    function NftOffers(uint _auctionListCount, string memory _name, uint _price) external {
+        address contractAddress = auctionListCount[_auctionListCount].contractAddress;
+        uint tokenId = auctionListCount[_auctionListCount].tokenId;
+        require(NftAuction[contractAddress][tokenId].owner != msg.sender, "You are Not Eligible for Bidding");
+        require(NftAuction[contractAddress][tokenId].isActive, "Not Listed In Offers!");
+        require(_price > NftAuction[contractAddress][tokenId].highestBid, "Bid must be higher than current highest bid");
+        NftAuction[contractAddress][tokenId].highestBid = _price;
+        NftAuction[contractAddress][tokenId].highestBidder = msg.sender;
+        NftAuction[contractAddress][tokenId].endTime = block.timestamp + 10 minutes;
+        Bidding[contractAddress][tokenId][userListCount[_auctionListCount] + 1] = userDetail(msg.sender, _name, _price, block.timestamp, userListCount[_auctionListCount] + 1);
+        userBiddingCount[contractAddress][tokenId][msg.sender]++;
+        userListCount[_auctionListCount]++;
+    }
 }
